dc6c3631517bb3660ae47846110fdb76809ac265
Katie <katie@imagings-MacBook-Pro.local>
2018-06-20 15:32:08 -0700

    JS is getting there

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,30 +1,11 @@
-// create a timer
 
-function countDown() {
-    var element = $("#timer");
-    element.text("You have " + counter + " seconds to answer");
-    if (counter < 1) {
-        counter = 10;
-        clearInterval(timer);
-        setTimeout(renderQuestion, 5000);
-        
-        // show correct answer
-        // advance to next question
-        // add to unanswered questions
-    }
-    counter--;
-    // timer = setTimeout('countDown(' + secs + ', "' + "#timer" + '")', 1000);
 
-}
+// Global variables
 
-// countDown(10, "#timer");
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0, incorrect = 0, unanswered = 0, counter = 10, timer, canClick = false;
 
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0, incorrect = 0, unanswered = 0, counter = 10, timer;
-// var valueA = $("#testa").text(chA);
-// var valueB = $("#testb").text(chB);
-// var valueC = $("#testc").text(chC);
-// var valueD = $("#testd").text(chD);
 var guessed = [];
+var hasAnswered = false;
 
 var questions = [
     [ "What Band Sang a Song called 'Robert De Niro's Waiting'?", "The Go-Go's", "The Eurythmics", "Bananarama", "The Bangles", "C", "Bananarama"  ],
@@ -39,20 +20,48 @@ var questions = [
     [ "What record was on the Number 1 list the longest in 1982?", "Asia", "Freeze Frame", "Beauty and the Beat", "For Those About to Rock We Salute You", "A", "Asia"]
 
 ];
+// create a timer
 
+function countDown() {
+    var element = $("#timer");
+    element.text("You have " + counter + " seconds to answer");
+    if (counter < 1) {
+        //counter = 10;
+        $("#containerTest").hide();
+        $("#answerStatus").show().text("The correct answer was " + questions[pos][6]);
+        canClick = false;
+        unanswered++
+        pos++
+        // $("#answerStatus").text("The correct answer was " + questions[pos][6]);
+        clearInterval(timer);
+        setTimeout(renderQuestion, 5000);
+        
+        // show correct answer
+        // advance to next question
+        // add to unanswered questions
+    }
+    counter--;
+    
+}
+// create the questions
 function renderQuestion() {
+    $("#answerStatus").hide();
+    $("#containerTest").show();
+    canClick = true;
     counter = 10;
     timer = setInterval(countDown, 1000);
     test = $("#test");
-    if (pos >= questions.length){
+
+    if (pos >= questions.length) {
         // show total correct, total incorrect and total unanswered in HTML
         // show start over button
         // if user clicks startover then
         pos = 0;
-        renderQuestion();
+        
+        //renderQuestion();
         // without reloading the game
     }
-    // $("#test_status").text("Question " + (pos + 1) + " of " + questions.length);
+    
     question = questions[pos][0];
     chA = questions[pos][1];
     chB = questions[pos][2];
@@ -64,41 +73,54 @@ function renderQuestion() {
     valueC = $("#testc").text(chC);
     valueD = $("#testd").text(chD);
     
-    
+}  
 
-}
+
 function checkAnswer() {
 
     $(".choices").on("click", function () {
         var choices = $(this).attr("value");
+        hasAnswered = true;
+        
         
         for (var i = 0; i < choices.length; i++) {
             if (choices[i] === questions[pos][5]) {
                 console.log(guessed);
                 choice = choices[i].value;
                 correct++;
-                alert("you guessed it");
-                
+                $("#containerTest").hide();
+                $("#answerStatus").show();
+                $("#answerStatus").text("You guessed correctly!");
             }
             else {
-                alert("nope not that one, the correct answer was " + questions[pos][6]);
+                $("#containerTest").hide();
+                $("#answerStatus").show();
+                $("#answerStatus").text("Nope not that one, the correct answer was " + questions[pos][6]);
                 incorrect++
             }
         }
         
         pos++;
         clearInterval(timer);
+        //setTimeout(checkAnswer, 3000);
         setTimeout(renderQuestion, 5000);
-        //countDown(10, "#timer");
-
+        
     });
-
+// if (hasAnswered === false) {
+//     clearInterval(timer);
+//     setTimeout(renderQuestion, 5000);
+//     unanswered++;
+// }
+// function startGame() {
+//     $(".containerQuestions").hide();
+//     $
+// };
 
 
 }
 $(document).ready(function () {
     renderQuestion();
-    //countDown(10, "#timer");
+   
     checkAnswer();
 
 });
